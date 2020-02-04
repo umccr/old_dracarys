5cc8d58575573a846a5f9199867eb335b93be827
pdiakumis <pdiakumis@unimelb.edu.au>
2020-02-04 17:28:12 +1100

    read DRAGEN version file

---
+++ R/dragen_settings.R
@@ -0,0 +1,41 @@
+
+
+#' Read Replay File
+#'
+#' Reads the `replay.json` file, which contains the DRAGEN command line,
+#' parameters, version and inputs for the specific run.
+#'
+#' @param x Path to `replay.json` file.
+#'
+#' @return A list with the following elements:
+#'   - `command_line`: character. DRAGEN command line used.
+#'   - `dragen_config`: tibble. Parameters used for the DRAGEN run.
+#'   - `inputs`: tibble. Germline input FASTQ files.
+#'   - `system`: list. DRAGEN version, node name, and kernel release.
+#'
+#'
+#' @examples
+#' x <- system.file("extdata/COLO829-replay.json.gz", package = "dracarys")
+#' read_replay(x)
+#'
+#' @export
+read_replay <- function(x) {
+
+  res <- x %>%
+    jsonlite::read_json(simplifyVector = TRUE) %>%
+    purrr::map_if(is.data.frame, tibble::as_tibble)
+  stopifnot(length(res) == 4,
+            all(names(res) %in% c("command_line", "dragen_config", "inputs", "system")))
+
+  res$dragen_config <- res$dragen_config %>%
+    dplyr::mutate(value = ifelse(grepl("https", .data$value), "pre-signed URL maybe?", .data$value),
+                  value = ifelse(name == "vc-decoy-contigs", "lots of contigs", .data$value)) %>%
+    dplyr::filter(!grepl("credentials", name)) %>%
+    dplyr::arrange(name)
+
+  res$system <- res$system %>%
+    tibble::as_tibble() %>%
+    tidyr::pivot_longer(dplyr::everything())
+
+  res
+}
