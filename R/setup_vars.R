f340767648d099673e6f0b267e2a29965c68d46f
pdiakumis <pdiakumis@unimelb.edu.au>
2020-02-26 01:11:48 +1100

    read files via setup vars

--- R/setup_vars.R
+++ R/setup_vars.R
@@ -49,22 +49,23 @@ setup_vars <- function(x) {
     ""
   }
 
-  list(
-    replay_fn = list(fn = file.path(ri$resdir, glue::glue("{nm}-replay.json")), lab = nm),
-    fraglen_fn = list(fn = file.path(ri$resdir, glue::glue("{nm}.fragment_length_hist.csv")), lab = nm),
-    map_met_fn = list(fn = file.path(ri$resdir, glue::glue("{nm}.mapping_metrics.csv")), lab = nm),
-    ploidy_est_fn = list(fn = file.path(ri$resdir, glue::glue("{nm}.ploidy_estimation_metrics.csv")), lab = nm),
-    time_met_fn = list(fn = file.path(ri$resdir, glue::glue("{nm}.time_metrics.csv")), lab = nm),
+  l <- list(
+    replay_fn = file.path(ri$resdir, glue::glue("{nm}-replay.json")),
+    fraglen_fn = file.path(ri$resdir, glue::glue("{nm}.fragment_length_hist.csv")),
+    map_met_fn = file.path(ri$resdir, glue::glue("{nm}.mapping_metrics.csv")),
+    ploidy_est_fn = file.path(ri$resdir, glue::glue("{nm}.ploidy_estimation_metrics.csv")),
+    time_met_fn = file.path(ri$resdir, glue::glue("{nm}.time_metrics.csv")),
 
-    cov_contig_fn = list(fn = file.path(ri$resdir, glue::glue("{nm}.wgs_contig_mean_cov{suffix}.csv")), lab = paste0(nm, suffix)),
-    cov_met_fn = list(fn = file.path(ri$resdir, glue::glue("{nm}.wgs_coverage_metrics{suffix}.csv")), lab = paste0(nm, suffix)),
-    cov_finehist_fn = list(fn = file.path(ri$resdir, glue::glue("{nm}.wgs_fine_hist{suffix}.csv")), lab = paste0(nm, suffix)),
+    cov_contig_fn = file.path(ri$resdir, glue::glue("{nm}.wgs_contig_mean_cov{suffix}.csv")),
+    cov_met_fn = file.path(ri$resdir, glue::glue("{nm}.wgs_coverage_metrics{suffix}.csv")),
+    cov_finehist_fn = file.path(ri$resdir, glue::glue("{nm}.wgs_fine_hist{suffix}.csv")),
 
     vc_met_fn = dplyr::if_else(ri$vc, file.path(ri$resdir, glue::glue("{nm}.vc_metrics.csv")), NA_character_),
     snv_fn = dplyr::if_else(ri$vc, file.path(ri$resdir, glue::glue("{nm}.hard-filtered.vcf.gz")), NA_character_),
     sv_fn = dplyr::if_else(ri$sv, file.path(ri$resdir, glue::glue("{nm}.sv.vcf.gz")), NA_character_),
-    info = ri,
-    label = nm
+    info = ri
   )
+  res <- purrr::set_names(list(l), nm)
+  res
 }
 
