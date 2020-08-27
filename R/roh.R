1579d41e2911257f48046614febd52b310f83630
pdiakumis <pdiakumis@unimelb.edu.au>
2020-08-28 00:55:44 +1000

    read ROH file

---
+++ R/roh.R
@@ -0,0 +1,25 @@
+#' Read ROH Metrics File
+#'
+#' @param x Path to `roh_metrics.csv` file.
+#'
+#' @return A tibble with two columns:
+#'         - `pct_snv_in_3mb_roh`: percent of SNVs in large (>= 3Mb) ROH regions.
+#'         - `n_3mb_roh`: number of large (>= 3Mb) ROH regions.
+#'
+#' @examples
+#' x <- system.file("extdata/COLO829_N.roh_metrics.csv", package = "dracarys")
+#' (roh <- read_roh_metrics(x))
+#'
+#' @testexamples
+#' expect_equal(nrow(roh), 1)
+#'
+#' @export
+read_roh_metrics <- function(x) {
+  readr::read_csv(x, col_names = c("dummy1", "dummy2", "key", "value"),
+                  col_types = "cccd") %>%
+    dplyr::select(.data$key, .data$value) %>%
+    dplyr::mutate(key = ifelse(.data$key == "Percent SNVs in large ROH ( >= 3000000)", "pct_snv_in_3mb_roh",
+                               ifelse(.data$key == "Number of large ROH ( >= 3000000)", "n_3mb_roh", "OTHER"))) %>%
+    tidyr::pivot_wider(names_from = .data$key, values_from = .data$value)
+
+}
